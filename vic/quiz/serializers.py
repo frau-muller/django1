6b4d0e308fba918f1f73eaca57a9f166286aa0df
frau-muller <frau-muller@tut.by>
2021-10-26 22:31:10 +0300

    serial

--- vic/quiz/serializers.py
+++ vic/quiz/serializers.py
@@ -1,9 +1,14 @@
 from quiz.models import Quiz, QuizTaker, Question, Answer, UsersAnswer
 from rest_framework import serializers
 
+
 class QuizListSerializer(serializers.ModelSerializer):
 	questions_count = serializers.SerializerMethodField()
 	class Meta:
 		model = Quiz
 		fields = ["id", "name", "description", "image", "slug", "questions_count"]
 		read_only_fields = ["questions_count"]
+
+	def get_questions_count(self, obj):
+		return obj.question_set.all().count()
+
--- vic/vic/urls.py
+++ vic/vic/urls.py
@@ -13,10 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('nested_admin', include('nested_admin.urls'))
+    path('api/', include('quiz.urls')),
+    path('api/auth/', include('accounts.urls')),
+    path('nested_admin', include('nested_admin.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
